c7d65c6242c43f67afac5d98c9952288d6118167
jxpsert <jasperdgp@outlook.com>
2020-09-27 19:09:57 +0200

    Forces .fetch() now checks cache first

--- structures/ForcesManager.js
+++ structures/ForcesManager.js
@@ -4,45 +4,49 @@ const Force = require('./Force.js');
 const Officer = require('./Officer.js');
 
 class ForcesManager { // manages forces in a Client - <Client>.forces
-    constructor (){
+    constructor() {
         this.cache = new Collection();
     }
 
-    async fetch(id){
+    async fetch(id) {
         try {
-            let force = await Fetch(`https://data.police.uk/api/forces/${id}`);
-            let forceJSON = await force.json();
+            if (!this.cache.get(id)) {
+                let force = await Fetch(`https://data.police.uk/api/forces/${id}`);
+                let forceJSON = await force.json();
 
-            let seniors = await Fetch(`https://data.police.uk/api/forces/${id}/people`);
-            let seniorsJSON = await seniors.json();
-            
-            let seniorOfficers = new Collection();
+                let seniors = await Fetch(`https://data.police.uk/api/forces/${id}/people`);
+                let seniorsJSON = await seniors.json();
 
-            seniorsJSON.forEach(senior => {
-                let newOfficer = new Officer({
-                    "name":senior.name,
-                    "rank":senior.rank,
-                    "bio":senior.bio,
-                    "contact":senior.contact_details
+                let seniorOfficers = new Collection();
+
+                seniorsJSON.forEach(senior => {
+                    let newOfficer = new Officer({
+                        "name": senior.name,
+                        "rank": senior.rank,
+                        "bio": senior.bio,
+                        "contact": senior.contact_details
+                    });
+                    seniorOfficers.set(senior.name, newOfficer);
+                })
+
+                let forceObject = new Force({
+                    "id": forceJSON.id,
+                    "name": forceJSON.name,
+                    "description": forceJSON.description,
+                    "telephone": forceJSON.telephone,
+                    "engagement": forceJSON.engagement_methods,
+                    "url": forceJSON.url,
+                    "seniors": seniorOfficers
                 });
-                seniorOfficers.set(senior.name, newOfficer);
-            })
-
-            let forceObject = new Force({
-                "id":forceJSON.id,
-                "name":forceJSON.name,
-                "description":forceJSON.description,
-                "telephone":forceJSON.telephone,
-                "engagement":forceJSON.engagement_methods,
-                "url":forceJSON.url,
-                "seniors":seniorOfficers
-            });
-
+            } else {
+                return this.cache.get(id);
+            }
             this.cache.set(id, forceObject);
             return forceObject;
-        } catch(err) {
+        } catch (err) {
             console.error(err);
         }
     }
 }
+
 module.exports = ForcesManager;
---
+++ test.js
@@ -0,0 +1,3 @@
+const {convert} = require('convert');
+
+console.log(convert(4351501787).from('milliseconds').to('days'));
